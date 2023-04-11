428dc7338fdb07ad973512cbbae30930b532c3f2
Umarkhan0 <125694026+Umarkhan0@users.noreply.github.com>
2023-04-12 05:28:15 +0845

    Add files via upload

---
+++ ch 15 to 17/app.js
@@ -0,0 +1,193 @@
+//*********************************** */ CHAPTER NO 15
+                                        // QUESTION NO 1
+// var empty = [1,2, ,4];
+// console.log(empty);
+
+                                        // QUESTION NO 2
+// var student = new Array();
+// console.log(student)
+
+                                        // QUESTION NO 4
+// var num = ['0','1','2','3'];
+// console.log(num);
+           
+                                            // QUESTION NO 5
+// var bol[true];
+// console.log(bol)   
+
+                                                // QUESTION NO 6
+// var mixedArray = [201 ,"umar","khan",true];
+// console.log(mixedArray);
+ 
+                                                // QUESTION NO 7
+// var qulifica = ["SSC", "HSC","BCS","BS", "BCOM", "MS","M. Phil.", "PhD."];
+// document.write(qulifica);
+
+                                                    // QUESTION NO 8
+// var scor = ["score of umar 320. percentage: 64%","score of Ahsan 230. percentage: 46%",'score of Rafay 480. percentage: 96%'] 
+// document.write(scor);
+
+                                                    // QUESTION NO 9
+// var colors = ['<b> Red','Green','Blue </b>'];
+// document.write("original Array:"+colors+"<br>");
+
+// var addColors =prompt("Enter the color you want to add the beginnig of the array:");
+// colors.unshift(addColors);
+// document.write("update array (After adding color to the Begining):<b>" + colors +"</b> <br>");
+
+// var addColors = prompt("Enter the color you want to add to the end of array:");
+// colors.push(addColors);
+// document.write("update array (After adding color to the and):<b>"+colors+"</b> <br>");
+
+// colors.unshift("pink","white");
+// document.write("update array (after ading  two more colors to the begining):<b>" + colors + "</b><br>");
+
+// colors.shift()
+// document.write("update Array (After deleting first colors to the begining):<b>"+colors+"</b> <br>");
+
+// colors.pop();
+// document.write("Update Array (after deleting last colors to the and):<b>"+colors+"<b> <br>");
+
+// var index = prompt("Enter the index at which you want to add the color:","1");
+// addColors = prompt("Enter the color you went to add:");
+// colors.splice(index, 0 , addColors);
+// document.write("Update array (After Adding color at desired index):<b>"+colors+"</b> <br>");
+
+// index = prompt("Enter the index at which you went to delete color (s):");
+// var deletNumbersofcolors = prompt("Enter the number of colors you want to delete:");
+// colors.splice(index, deletNumbersofcolors);
+// document.write("update Array (After Deleting color(s)from desired index): <b>"+colors+"<b> <br>");
+
+                                                // QUESTION NO 10
+// var scors_studend =[",320",",230",",480",",120"];
+// scors_studend=(scors_studend[3]+scors_studend[1]+scors_studend[0]+scors_studend[2]);    
+// console.log(scors_studend)   
+
+                                                    // QUESTION NO 11
+// var cites =['Karachi','Lahore','Islamabad','Qutta','peshawar'];
+// var copy = cites.slice(2,4);
+// console.log(copy);
+
+                                                        // QUESTION NO 12
+//  var arr = ["This is my cat"];  
+// console.log(arr);
+
+                                                        // QUESTION NO 13
+// var fifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var fif = fifo.shift();
+// console.log(fif);       
+
+// var fifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// console.log(fif);       
+
+// var fifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// console.log(fif);       
+
+// var fifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// var fif = fifo.shift();
+// console.log(fif);       
+
+                                                        // QUESTION NO 14
+// var lifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var lif = lifo.pop();
+// console.log(lif);           
+
+// var lifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// console.log(lif); 
+
+// var lifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// console.log(lif); 
+
+// var lifo = ['Kibord,','Mouse,','Pirentor','Monitor'];
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// var lif = lifo.pop();
+// console.log(lif); 
+
+                                                            // QUESTION NO 15
+// var dropd = ['Apple','Samsung,','Motorola,','Nokia,','Sony & Haier','vevo'];
+// document.write('<h1> Mobile Manufacture comanies list: </h1> <select> <option>'+dropd.slice(0,1) +'</option>')
+// document.write('<option>'+dropd.slice(1,2) +'<option>')
+// document.write('<option>'+dropd.slice(2,3) +'<option>')
+// document.write('<option>'+dropd.slice(3,4) +'<option>')
+// document.write('<option>'+dropd.slice(4,5) +'<option>')
+// document.write('<option>'+dropd.slice(5,6) +'<option> </select>')
+
+                                            // CHAPTER NO 17
+                                                                // QUESTION NO 1
+// var arrarr =[[]];
+// console.log(arrarr);
+
+                                                                // QUESTION NO 2
+// var arrarr =[[0 ,1 ,2 ,3],[1, 0 ,1 ,2],[2, 1, 0, 1]];
+// document.write(arrarr);                         
+
+                                                                // QUESTION NO 3
+// for (var i= 0; i <=10; i++){
+//     document.write(i +'<br>')
+// }              
+
+                                                                // QUESTION NO 4
+// for(var i =1; i <= 12; i++){
+//     document.write('<h3>2*'+i +'='+ 2 * i +'</h3>')
+// }
+// var user_table =prompt ();
+// var user_lengt =prompt ()
+// document.write('<h3>Multiplication table = '+user_table+'<br>' +'Length = '+user_lengt+'</h3>')
+// for(var i = 1; i <= user_lengt; i++ ){
+//     document.write(user_table+"*"+i +'='+ user_table*i +'<br>')
+// }
+
+                                                                            // QUESTION NO 5
+// var fruts = ['apple','banana','mango','orenge','stawberry']
+// for(var i = 0; i <= 4; i++){
+//     document.write("Element at index "+ i +" is "+ fruts [i] + "<br>" )
+// }                 
+
+                                                                            // QUESTION NO 6
+// for(var i = 1; i <= 10; i++){
+//     document.write(i +',')
+// }
+
+                                                                            // QUESTION NO 7
+// var bakry_item = ['cake', 'apple pie', 'cookie', 'chips', 'patties'];
+// var user = prompt('Enter your food')  
+// var flag = '';
+// for(var j = 0; j < 4; j++){
+//     if(bakry_item[j] == user){
+//         flag = true;
+//     }
+// }
+// if(flag){
+//     alert(user + ' is availible at index '+' in our bakery ');
+// }
+// else{
+//     alert('We are sorry. ' +user+ 'is not available in our bakery ')
+// }
+
+                                                                    // QUESTION NO 8
+// var A = [24, 53, 78, 91, 12];
+// document.write('The largest number is '+ A [3] );
+
+                                                                // QUESTION NO 9
+// var A = [24, 53, 78, 91, 12];
+// document.write('The smallest number is ' + A [4]);
+
+                                                                // QUESTION NO 10
+// for(var i =1; i <= 20; i++){
+//     document.write('<b>'+ 5 * i +'<b>'+',')
+// }
