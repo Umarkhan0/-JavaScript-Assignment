3ac79f7aa5fafb34c5da47c7e0b48ebf404bb1b0
Umarkhan0 <125694026+Umarkhan0@users.noreply.github.com>
2023-04-12 05:33:01 +0845

    Add files via upload

---
+++ ch 26 to 30/app.js
@@ -0,0 +1,34 @@
+                                                // CH NO 26 TO 27
+                                                
+                                                // QUESTION NO 1
+
+// var round = 3.45214;
+// document.write("Value "+ round +"<br> round of value " + Math.round(round)+"<br> floor value "+Math.floor(round)+"<br>ceil value "+Math.ceil(round));
+
+                                                // QUESTION NO 2
+
+// var round = -2.673;
+// document.write("Value "+ round +"<br> round of value " + Math.round(round)+"<br> floor value "+Math.floor(round)+"<br>ceil value "+Math.ceil(round));
+
+                                                // QUESTION NO 3     
+
+// var firstDice = Math.random()*6;
+// var secondDice = Math.random()*6;                                        
+// document.write("ramdom dice value: " + Math.ceil(firstDice)); 
+// document.write("<br>ramdom dice value: " + Math.ceil(secondDice));
+
+                                                // QUESTION NO 4
+
+
+// var random = Math.random()*2;
+// var ramdomCeil = Math.ceil(random);
+// if(ramdomCeil == 1){
+//     alert("head")
+// }else if(ramdomCeil == "2"){
+//     alert("Tail")
+// }
+                                                // QUESTION NO 5
+
+   
+
+                                                                                             
