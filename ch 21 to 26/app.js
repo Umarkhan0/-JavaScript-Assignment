ee60f3bad7178c70a4bff1a380169b2e440c2915
Umarkhan0 <125694026+Umarkhan0@users.noreply.github.com>
2023-04-12 05:30:34 +0845

    Add files via upload

---
+++ ch 21 to 26/app.js
@@ -0,0 +1,139 @@
+                                        // CHAPTER NO 21-26
+                                        // QUESTION NO 1
+
+// var first_name =prompt("Enter your first name");
+// var second_name =prompt("Enter your second name");
+// var first_name_upper = first_name[0].toUpperCase() + first_name.slice(1)
+// var second_name_upper = second_name[0].toUpperCase()+second_name.slice(1)
+// var answer = first_name_upper+second_name_upper
+// document.write(answer+"<p>salam</p>")
+
+                                        // QUESTION NO 2
+
+// var userInput = prompt("What is your favorite mobile phone model?");
+// var userInputLength = userInput.length;
+// document.write("The length of your favorite mobile phone model is: " + userInputLength);
+
+                                        // QUESTION NO 3
+
+// var mixedCountry = ["Africa","America","Pakistan","Dubai"];
+// var user_input = prompt ("Enter your city");
+// var chaking = mixedCountry.indexOf(user_input);
+// document.write("String: "+user_input+" index of 'n' :"+chaking)
+
+//                                         // QUESTION NO 4
+// var hell = "Hello world";
+// var len = hell.length
+// document.write("String:" + hell +"<br/> last index of "+ hell.lastIndexOf("Hello world")+":"+len);
+
+//                                           QUESTION NO 5
+
+// var str = "pakistani";
+// var att = str.indexOf("i")
+// var ou =str[3];
+// document.write("String: "+str +"<br> Charecter at index" + att+":" + ou);
+
+//                                           QUESTION NO 7
+
+// var str = "Hadrabad";
+// var rep = str.replace("Hadrabad","Islamabad");
+// document.write("City: "+str+"<br> After replecment: "+rep)
+
+//                                           QUESTION NO 8
+// var message = "Ali and Sami are best friends. They play cricket andfootball together.";;
+// var messager = message.replace(/and/g,"&");
+// console.log(messager);
+
+                                            // QUESTION NO 9
+// var numberAsNumber = 1234;
+//  var numberAsStrings = numberAsNumber.toString();
+//  document.write("Value "+numberAsNumber +"<br> Type string"+" <br>Value "+numberAsNumber+"  <br>Type "+ " "+ typeof (numberAsNumber));
+
+                                            // QUESTION NO 10
+// var userInput = "peanuts";
+// document.write(userInput.toUpperCase());
+
+                                            // QUESTION NO 11
+// var input = "javascript";
+// var capsInput = input[0].toUpperCase() + input.slice(1);
+// document.write(capsInput);
+
+                                            // QUESTION NO 12
+// var num = 35.36;
+// var str = num.toString().replace(".", "");
+// document.write("<p>Number: " + num + "</p>");
+// document.write("<p>String: " + str + "</p>");
+
+                                            // QUESTION MO 13
+// var userName = prompt ("Enter your name");
+// for (var i = 0; i < userName.length; i++) {
+//     if (userName[i] === "!") {
+//     alert("Not allow for special");
+//     break;
+// }
+// else if (userName[i] === "@"){
+//     alert("Not allow for special")
+//     break;
+// }
+// else if (userName[i] === ","){
+//     alert("Not allow for special")
+//     break;
+// }
+// else if (userName[i] === "."){
+//     alert("Not allow for special")
+//     break;
+// }
+// else{
+//    alert("Thanks")
+//    break;
+// }
+// }
+
+                                            // QUESTION NO 14
+
+// var itemes = ["cake","apple pie","cookie","chips","patties"];
+//  var enterItem = prompt("Welcome to Shani's Bakery. What do you want to order sir/ma'am?");
+// var flag = false;
+// for (var i = 0; i < itemes.length; i++) {
+// if (enterItem === itemes[i]){
+// flag = true;
+// break;
+// }
+//  }
+                                            
+// if (flag === true) {
+//     document.write(enterItem + " is available at index " + i + " in our bakery");
+// }
+// else {
+//     document.write("We are sorry. " + enterItem + " is not available in our bakery");
+// }
+
+                                                                // QUESTION NO 15
+                                                                
+// var password_input =prompt("Enter a password");
+// var special = "@!(){}0123456789"
+// var userInputLength = 6;
+// var fl = true
+// for(var i = 0; i < special.length; i++){      
+//         if(password_input.length < userInputLength){
+//         alert("Password should be at least 6 characters long.")
+//         break;
+//     }else if(password_input < special[i]) {
+//         alert("Password should contain both alphabets and numbers.")
+//     }break;
+//     }
+    
+                                                                // QUESTION NO 16
+// var nam = "University of Karachi";     
+// var spl = nam.split("");
+// console.log(spl)
+                                                          
+                                                                // QUESTION NO 17
+// var userInput = prompt("Enter at name");
+// var index = userInput[userInput.length - 1];
+// console.log(index)                                                               
+
+                                                                // QUESTION NO 18
+                                                               
+
+                                                                
